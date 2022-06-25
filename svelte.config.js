743042623485af6d6cd2cf0c18801016e2a34cde
franco-giordano <31712363+franco-giordano@users.noreply.github.com>
2022-06-25 02:02:00 -0300

    fixed gh pages publishing

--- svelte.config.js
+++ svelte.config.js
@@ -8,14 +8,18 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-	adapter: adapter({
-		pages: 'docs',
-		assets: 'docs',
-	}),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+		}),
+		paths: {
+			base: dev ? '' : '/trencito',
+		},
+		prerender: {
+			default: true
+		}
 	},
-	base: dev ? '' : '/trencito',
 };
 
 export default config;
