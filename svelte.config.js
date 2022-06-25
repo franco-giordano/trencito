801c85c405df7038c3126acf4176843d82509205
franco-giordano <31712363+franco-giordano@users.noreply.github.com>
2022-06-25 01:08:20 -0300

    test gh pages deploy'

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static";
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
+	adapter: adapter({
+		pages: 'docs',
+		assets: 'docs',
+	}),
 	kit: {
 		adapter: adapter()
-	}
+	},
+	base: dev ? '' : '/trencito',
 };
 
 export default config;
